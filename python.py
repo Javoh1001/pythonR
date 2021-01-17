af8bc6a425e394ad820dbc036b437f890f455134
Javoh1001 <javoh1001@gmail.com>
2021-01-17 10:51:18 +0300

    python commt

---
+++ python.py
@@ -0,0 +1,15 @@
+a = input('a ni kirit: ')
+b = input('b ni kirit: ')
+d = input('d ni kirit: ')
+
+if a > b:
+    c = a
+elif a < b:
+    c = b
+if c > d:
+    k = c
+elif c < d:
+    k = d
+
+print(k)
+
